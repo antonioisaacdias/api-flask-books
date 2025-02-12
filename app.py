b50a66d8545d37ce9bcba3325282875a36d749d2
antonioisaacdias <antonioisaacvd@gmail.com>
2025-02-12 00:00:14 -0300

    Rota DELETE para exclusão de livros através do UUID

--- app.py
+++ app.py
@@ -38,5 +38,12 @@ def update_book(book_id):
     db.session.commit()
     return jsonify({'message': 'Livro atualizado com sucesso'})
 
+@app.route('/books/<uuid:book_id>', methods=['DELETE'])
+def delete(book_id):
+    book = Book.query.get_or_404(book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'message': 'Livro removido com sucesso'})
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
